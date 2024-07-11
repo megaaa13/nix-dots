983b0ad5dffc63073c0e6bcc82198f01ac2a82e3
megaaa13 <martin.gyselinck02@gmail.com>
2024-07-11 02:28:43 +0200

    First draft of working Hyprland config

---
+++ nixos/configuration.nix
@@ -0,0 +1,146 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+      ./start.nix
+      ./secureboot.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Brussels";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "fr";
+    xkb.variant = "azerty";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+ 
+ 
+  users.defaultUserShell = pkgs.zsh;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.martin = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "input" ];
+  };
+
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+	enable = true;
+	alsa.enable = true;
+	alsa.support32Bit = true;
+	pulse.enable = true;
+	jack.enable = true;
+  };
+ 
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+	neovim
+	libnotify
+	firefox
+	thunderbird
+	wget
+	vscode
+	zsh
+	home-manager
+	git
+	spotify
+	discord
+	pipewire
+	wireplumber
+	grim
+	slurp
+	sbctl
+  ];
+
+  fonts.packages = with pkgs; [
+      nerdfonts
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  programs.hyprland = {
+	enable = true;
+	xwayland.enable = true;
+  };
+  
+  programs.zsh.enable = true;
+
+  environment.sessionVariables = {
+	WLR_NO_HARDWARE_CURSOR = "1";
+	NIXOS_OZONE_WL = "1";
+  };
+  
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware = {
+	opengl.enable = true;
+	nvidia.modesetting.enable = true;
+	nvidia.powerManagement.enable = false;
+	nvidia.powerManagement.finegrained = false;
+	nvidia.open = false;
+	nvidia.nvidiaSettings = true;
+	nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
---
+++ nixos/start.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  session = "${pkgs.hyprland}/bin/Hyprland";
+  username = "martin";
+in
+
+{
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "${session}";
+        user = "${username}";
+      };
+      default_session = {
+        command = "${tuigreet} --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time -cmd ${session}";
+        user = "greeter";
+      };
+    };
+  };
+}
