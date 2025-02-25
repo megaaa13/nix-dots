985cbf6c6e7fce19f89f0f196c0cea4dbcc08a93
megaaa13 <martin.gyselinck02@gmail.com>
2025-02-25 12:40:24 +0100

    No more package build itself

--- home/hyprland/hypr/default.nix
+++ home/hyprland/hypr/default.nix
@@ -15,8 +15,6 @@ in
   config = lib.mkIf hyprlandcfg.enable {
     wayland.windowManager.hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       systemd = {
         enable = true;
         enableXdgAutostart = true;
