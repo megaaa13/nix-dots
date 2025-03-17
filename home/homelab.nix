7cf240afbd9f52466bb82497b10ba492344b59ad
megaaa13 <martin.gyselinck02@gmail.com>
2025-03-17 12:36:47 +0100

    [UPDATE] Direnv on homelab

--- home/homelab.nix
+++ home/homelab.nix
@@ -51,6 +51,12 @@
     tmux
   ];
 
+  programs.direnv = {
+        enable = true;
+        enableBashIntegration = true; # see note on other shells below
+        nix-direnv.enable = true;
+  };
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
