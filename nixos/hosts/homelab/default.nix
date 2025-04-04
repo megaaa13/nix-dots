bab6b7455fe05c6871e0ec79444cf9c2e998cbb4
megaaa13 <martin.gyselinck02@gmail.com>
2025-04-04 11:27:30 +0200

    [UPDATE] X11 forwarding

--- nixos/hosts/homelab/default.nix
+++ nixos/hosts/homelab/default.nix
@@ -115,6 +115,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = false;
+  services.openssh.settings.X11Forwarding = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
